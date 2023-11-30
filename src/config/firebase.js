0ce24ae4a481f15edc661fb0b8f5df39c1c3f705
donghun <iandonghun@gmail.com>
2023-11-30 18:54:43 +0900

    Add Todo 00 for firebase setup

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// TODO: 00 - Create a Firebase project & Set up your app
+// Example: .env.local file
+// REACT_APP_API_KEY=
+// REACT_APP_AUTH_DOMAIN=
+// REACT_APP_PROJECT_ID=
+// REACT_APP_STORAGE_BUCKET=
+// REACT_APP_MESSAGING_SENDER_ID=
+// REACT_APP_APP_ID=
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
